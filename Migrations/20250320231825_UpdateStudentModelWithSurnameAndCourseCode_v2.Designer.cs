28044c066a9dbc297107b47dcb741fdb05405dd6
Ashley-sdev <muhlentuli0223@gmail.com>
2025-03-21 01:23:31 +0200

    Update Student model with Surname and CourseCode columns, and apply migration

--- Migrations/20250320231825_UpdateStudentModelWithSurnameAndCourseCode_v2.Designer.cs
+++ Migrations/20250320231825_UpdateStudentModelWithSurnameAndCourseCode_v2.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentPerformanceAPI.Data;
 
@@ -10,9 +11,11 @@ using StudentPerformanceAPI.Data;
 namespace StudentPerformanceAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250320231825_UpdateStudentModelWithSurnameAndCourseCode_v2")]
+    partial class UpdateStudentModelWithSurnameAndCourseCode_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250320231825_UpdateStudentModelWithSurnameAndCourseCode_v2.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPerformanceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStudentModelWithSurnameAndCourseCode_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseCode",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Students",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseCode",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
