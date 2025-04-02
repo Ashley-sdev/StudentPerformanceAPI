007bb35a3f97feb13967d126beb8d6f463fc13ed
Ashley-sdev <muhlentuli0223@gmail.com>
2025-04-02 22:20:59 +0200

    Update ApplicationDbContext, Student and Program classes

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,1 +1,16 @@
- 
+using Microsoft.EntityFrameworkCore;
+using StudentPerformanceAPI.Models;
+
+namespace StudentPerformanceAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add a DbSet for students
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,1 +1,19 @@
- 
+namespace StudentPerformanceAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CourseCode { get; set; }
+
+        // New properties
+        public int AssignmentMarks { get; set; }
+        public int TestMarks { get; set; }
+        public int ExamMarks { get; set; }
+
+        public double Average => (AssignmentMarks + TestMarks + ExamMarks) / 3.0;
+
+        public bool QualifiesForExam => Average >= 50; // pass mark is 50
+    }
+}
